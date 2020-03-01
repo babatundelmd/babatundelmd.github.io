80f15ca04b11a2b79943b0df81655855ba1b95a7
Babatunde Lamidi <odunayohlamidi@gmail.com>
2020-03-01 13:13:43 +0100

    refactor cog css

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const checkBox = document.querySelector( "input[name=check-box]" );
 
 checkBox.addEventListener( 'change', ( e ) =>
 {
+
     if ( e.checked = true ) {
+        console.log( e )
         const container = document.getElementById( "container-one" );
         const containertwo = document.getElementById( "container-two" );
-        container.classList.toggle( 'darktheme' )
-        containertwo.classList.toggle( 'darktheme' )
-
+        const svg = document.getElementById( "svg" );
+        container.classList.toggle( 'darktheme' );
+        containertwo.classList.toggle( 'darktheme' );
+        svg.classList.toggle( 'white-cog' )
     }
 } )
